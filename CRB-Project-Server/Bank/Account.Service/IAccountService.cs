21518299bf9e5a3b021febe3db865d04ceddb30e
ChanaBrenig <shchana63@gmail.com>
2022-09-04 15:48:46 +0300

    add post customer Api

---
+++ CRB-Project-Server/Bank/Account.Service/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using Account.Service.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account.Service
+{
+    public interface IAccountService
+    {
+        Task<bool> AddCustomerAsync(CustomerDTO customerDTO);
+    }
+}
---
+++ CRB-Project-Server/Bank/Account.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using Account.Service;
+using Account.Service.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Account.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+        // GET: api/<AccountController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AccountController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AccountController>
+        [HttpPost("AddCustomer")]
+        public async Task<ActionResult> AddCustomerAsync([FromBody] CustomerDTO customerDTO)
+        {
+            return Ok(await _accountService.AddCustomerAsync(customerDTO));
+        }
+
+        
+    }
+}
